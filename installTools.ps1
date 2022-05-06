efbd51ffe30ee0c5e74981222c155cf0cd0ced9c
Javier Soriano <javiersoriano@users.noreply.github.com>
2022-05-06 13:11:03 +0200

    adding Sentinel workspace

--- installTools.ps1
+++ installTools.ps1
@@ -7,3 +7,7 @@ Expand-Archive -Path .\LightIngest.zip -DestinationPath LightIngest
 Invoke-WebRequest -Uri https://aka.ms/downloadazcopy-v10-windows -OutFile AzCopy.zip -UseBasicParsing
 
 Expand-Archive -Path .\AzCopy.zip -DestinationPath AzCopy
+
+Invoke-WebRequest -Uri https://aka.ms/customlogsingestion -OutFile Send-AzMonitorCustomLogs.zip -UseBasicParsing
+
+Expand-Archive -Path .\Send-AzMonitorCustomLogs.zip -DestinationPath Send-AzMonitorCustomLogs
